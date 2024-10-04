fb770e6ab37f4cff24bb128ebf600042708b15e4
Joonatan Merenluoto <joonatan.merenluoto@gmail.com>
2024-10-04 18:27:08 +0300

    Välicommit

--- src/main/java/fi/utu/tech/ooj/exercise3/asiakasrekisteriohjelma/AsiakasRekisteriohjelmanKaynnistin.java
+++ src/main/java/fi/utu/tech/ooj/exercise3/asiakasrekisteriohjelma/AsiakasRekisteriohjelmanKaynnistin.java
@@ -1,16 +1,56 @@
 package fi.utu.tech.ooj.exercise3.asiakasrekisteriohjelma;
 
+import java.util.ArrayList;
+import java.util.UUID;
+
 public class AsiakasRekisteriohjelmanKaynnistin {
 
     public static void kaynnistaOhjelma() {
         System.out.println("------ Asiakas- ja tuoterekisteriohjelman demo käynnistyy ------");
-
+        Asiakasrekisteri a = new Asiakasrekisteri();
         /*
         * Tämä rutiini suoritetaan automaattisesti, kun tehtäväpohja suoritetaan
         *
         * Lisää tähän Asiakas- ja tuoterekisteriohjelman demonstrointiin tarvittava koodi
         *
         * */
+
+        class Asiakasrekisteri{
+
+            public ArrayList<Asiakas> asiakkaat = new ArrayList<Asiakas>();
+            public Asiakasrekisteri(){}
+
+            public void poistaAsiakas(long asiakasnumero){
+                for(Asiakas a : asiakkaat){
+                    if(a.getAsiakasnumero() == asiakasnumero){
+
+                    }
+                }
+            }
+
+        }
+        class Asiakas{
+            private String nimi;
+            private String katuosoite;
+            private String postinumero;
+            private String postitoimipaikka;
+            private String puhelinnumero;
+            private final long asiakasnumero;
+
+            public Asiakas(String nimi, String katuosoite, String postinumero, String postitoimipaikka, String puhelinnumero){
+                this.nimi = nimi;
+                this.katuosoite = katuosoite;
+                this.postinumero = postinumero;
+                this.postitoimipaikka = postitoimipaikka;
+                this.puhelinnumero = puhelinnumero;
+                this.asiakasnumero =  UUID.randomUUID().getMostSignificantBits() & Long.MAX_VALUE;
+            }
+            public long getAsiakasnumero(){
+                return this.asiakasnumero;
+            }
+        }
+
+
 
         System.out.println("------ Asiakas- ja tuoterekisteriohjelman demo päättyy ------");
     }
